70e4da3c63e39bb449f4c8e43c8f58a909fcbfee
AnanyaMad06 <ananyassmadankar@gmail.com>
2026-01-28 09:39:52 +0530

    Delete Allocation directory

--- Allocation/test_allocation.py
+++
@@ -1,9 +0,0 @@
-from allocator import allocate_police
-
-total_police = 50
-intensities = [2.0, 2.0, 3.0, 4.0, 5.0]
-
-result = allocate_police(total_police, intensities, min_police=5)
-
-print("🚓 Police Allocation:", result)
-print("🔢 Total Allocated:", sum(result))
